8bd370cc1f3ed3a12469269be9086b9f3d11bfaf
gerson434 <78971376+gerson434@users.noreply.github.com>
2022-10-24 13:52:32 -0500

    controladores

--- SGPI/Controllers/AdministradorController.cs
+++ SGPI/Controllers/AdministradorController.cs
@@ -6,11 +6,45 @@ namespace SGPI.Controllers
 {
     public class AdministradorController : Controller
     {
-        SGPIDBContext context = new SGPIDBContext();
-        public IActionResult Login()
-        {
-;
 
+        SGPIDBContext context = new SGPIDBContext();
+        [HttpPost]
+      public IActionResult Login(Usuario user){
+
+            var usuario = context.Usuarios
+                .Where(consulta => consulta.NumeroDocumento == user.NumeroDocumento
+                && consulta.Password == user.Password).FirstOrDefault();
+
+            if (usuario != null)
+            {
+                //Redirige a la vista principal administrador
+                if (usuario.IdRol == 1)
+                {
+                    return View("BuscarUsuario");
+                        }
+                //Redirige a la vista principal Coordinador
+                else if (usuario.IdRol == 2)
+                {
+                    return Redirect("Coordinador/BuscarEstudiante");
+                }
+                //Redirige a la vista principal Estudiante
+                else if (usuario.IdRol == 3)
+                {
+                    return Redirect("Estudiante/ActualizarEstudiante");
+                }
+                else
+                {}
+
+
+            }
+
+                return View();
+            }
+      
+
+            public IActionResult Login()
+        {
+            
             return View();
         }
         public IActionResult OlvidarContrasena()
