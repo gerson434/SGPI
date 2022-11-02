19ec96f68dee6273e935e11deffd97beb60e59f5
PC <PC@DESKTOP-3A4HF5H>
2022-11-01 22:04:11 -0600

    Nueva Actualizacion buscar

--- SGPI/Controllers/AdministradorController.cs
+++ SGPI/Controllers/AdministradorController.cs
@@ -7,7 +7,37 @@ namespace SGPI.Controllers
     public class AdministradorController : Controller
     {
         SGPIDBContext context = new SGPIDBContext();
-        public IActionResult Login()
+
+        [HttpPost]
+        public IActionResult Login(Usuario user)
+        {
+            var usuario = context.Usuarios
+               .Where(consulta => consulta.NumeroDocumento == user.NumeroDocumento
+               && consulta.Password == user.Password).FirstOrDefault();
+
+            if (usuario != null)
+            {
+                if (usuario.IdRol == 1)
+                {
+                    return Redirect("Administrador/BuscarUsuario");
+                }
+                else if (usuario.IdRol == 2)
+                {
+                    return Redirect("Coordinador/BuscarEstudiante");
+                }
+                else if (usuario.IdRol == 3)
+                {
+                    return Redirect("Estudiante/ActualizarEstudiante");
+                }
+                else
+                {
+
+                }
+            }
+            else { }
+            return View();
+        }
+            public IActionResult Login()
         {
 
             return View();
@@ -18,11 +48,73 @@ namespace SGPI.Controllers
         }
         public IActionResult CrearUsuario ()
         {
+            ViewBag.genero = context.Generos.ToList();
+            ViewBag.rol = context.Rols.ToList();
+            ViewBag.documento = context.Documentos.ToList();
+            ViewBag.programa = context.Programas.ToList();
+            
             return View();
+        }
+        [HttpPost]
+        public IActionResult CrearUsuario(Usuario usuario)
+        {
+            context.Usuarios.Add(usuario);
+            context.SaveChanges();
+
+            ViewBag.genero = context.Generos.ToList();
+            ViewBag.rol = context.Rols.ToList();
+            ViewBag.documento = context.Documentos.ToList();
+            ViewBag.programa = context.Programas.ToList();
+            return View();
+
         }
         public IActionResult BuscarUsuario()
         {
+            ViewBag.documento = context.Documentos.ToList();
             return View();
+        }
+        [HttpPost]
+        public IActionResult BuscarUsuario(Usuario usuario)
+        {
+            var us = context.Usuarios
+                            .Where(u => u.NumeroDocumento == usuario.NumeroDocumento
+                            && u.IdDoc == usuario.IdDoc).FirstOrDefault();
+
+            if (us != null)
+            {
+
+                ViewBag.documento = context.Documentos.ToList();
+
+                return View(us);
+            }
+            else
+            {
+                ViewBag.documento = context.Documentos.ToList();
+                return View();
+            }
+        }
+        public IActionResult MenuAdmModificar(int? IdUsuario)
+        {
+            Usuario usuario = context.Usuarios.Find(IdUsuario);
+            if (usuario != null)
+            {
+                return View(usuario);
+            }
+            else
+                return Redirect("Administrador/BuscarUsuario");
+        }
+        [HttpPost]
+        public IActionResult MenuAdmModificar(Usuario user)
+        {
+            context.Update(user);
+            context.SaveChanges();
+            return Redirect("Administrador/BuscarUsuario");
+        }
+        public IActionResult Delete(Usuario usuario)
+        {
+            context.Remove(usuario);
+            context.SaveChanges();
+            return Redirect("BuscarUsuario");
         }
         public IActionResult ReportesAdmin()
         {
--- SGPI/Models/SGPIDBContext.cs
+++ SGPI/Models/SGPIDBContext.cs
@@ -34,7 +34,7 @@ namespace SGPI.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-N7TM1CP\\SQLEXPRESS;Database=SGPIDB;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=DESKTOP-3A4HF5H\\SQLEXPRESS;Database=SGPIDB;Trusted_Connection=True;");
             }
         }
 
